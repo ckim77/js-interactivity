9f719d78868ee5a792c7ea5b6304370249f2625b
Chris Kim <ckjr92@gmail.com>
2021-10-27 15:34:11 -0600

    finished step 5

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+let message = document.querySelector("#message");
+
 
 const addMovie = event => {
     event.preventDefault();
@@ -6,6 +8,8 @@ const addMovie = event => {
     let movieTitle = document.createElement('span');
     movieTitle.textContent = inputField;
     movie.appendChild(movieTitle);
+    document.querySelector('ul').appendChild(movie);
+
 
     //step 3 (remove movies)
     let deleteBtn = document.createElement("button");
@@ -13,12 +17,11 @@ const addMovie = event => {
     deleteBtn.addEventListener('click', deleteMovie);
     movie.appendChild(deleteBtn);
 
-    //back to step2
-    document.querySelector('ul').appendChild(movie);
-    inputField = "";
 
     //step 4 (crossing out movies)
-    
+    movieTitle.addEventListener('click', crossOffMovie);
+
+    document.querySelector('input').value = "";
 }
 
 let form = document.querySelector('form');
@@ -27,17 +30,39 @@ form.addEventListener('submit', addMovie);
 
 const deleteMovie = event => {
     event.target.parentNode.remove();
+    message.textContent = "Movie deleted!";
 }
 
 // step 4
 const crossOffMovie = event => {
-    event.target.classList.toggle();
+    event.target.classList.toggle("checked");
+    if (event.target.classList.contains('checked')) {
+        message.textContent = 'Movie watched!';
+    } else {
+        message.textContent = "Movie added back!"
+    }
 }
 
 
 
-// console.log(document.createElement('list'));
+// let themeBtns = document.querySelectorAll('.theme-buttons')
 
+// const selectTheme = (event) => {
+//     let theme = event.target.textContent;
+//     document.querySelector('body').className = theme;
+//     document.querySelector('main').className = theme;
+//     let btns = document.querySelectorAll('button');
+
+//     for (let i = 0; i < btns.length; i++) {
+//         btns[i].className = theme;
+//     }
+// }
+
+// for  (let i = 0; i < themeBtns.length; i++) {
+//     themeBtns[i].addEventListener('click', () => {
+//         selectTheme(event);
+//     })
+// }
 
 
 
